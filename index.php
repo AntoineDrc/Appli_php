a334ee401b93f6940805562da7e7207ebd66dd7b
Antoine Delubriac <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-03-19 10:02:18 +0100

    1st commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+        <title>Ajout Produit</title>
+    </head>
+    <body>
+        
+        <h1 class="display-1">Ajouter un produit</h1>
+        <form action="traitement.php" method="post">
+            <p>
+                <label>
+                    Nom de produit : 
+                    <input type="text" name="name">
+                </label>
+            </p>
+            <p>
+                <label>
+                    Prix du produit : 
+                    <input type="number" step="any" name="price">
+                </label>
+            </p>
+            <p>
+                <label>
+                    Quantité désirée : 
+                    <input type="text" name="qtt" value="1">
+                </label>
+            </p>
+            <p>
+                <input type="submit" name="submit" value="Ajouter le produit" class="btn btn-primary">
+            </p>
+        </form>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    </body>
+</html>
