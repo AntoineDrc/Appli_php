fc6d82b3e7d50f3cb8268853e6c0bff6ad7e73b9
Antoine Delubriac <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-03-20 18:53:07 +0100

    ajout de commentaires

--- recap.php
+++ recap.php
@@ -1,7 +1,6 @@
 <?php 
-
+    // Démarre ou reprend une session existante
     session_start();
-
 ?>
 
 <!DOCTYPE html>
@@ -9,12 +8,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Inclut les styles Bootstrap pour styliser la page -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Récapitulatif des produits</title>
 </head>
 <header>
+    <!-- Navigation avec Bootstrap pour un meilleur rendu visuel -->
     <nav class="navbar bg-body-tertiary">
         <form class="container-fluid justify-content-start">
+            <!-- Boutons pour naviguer entre les pages d'ajout de produit et de récapitulatif du panier -->
             <a href="index.php" class="btn btn-primary me-2" type="button">Ajouter produit</a>
             <a href="recap.php" class="btn btn-primary" type="button">Panier</a>
         </form>
@@ -23,24 +25,27 @@
 <body>
     <?php
     
+    // Vérifie si le tableau 'products' dans la session est défini et non vide
     if(!isset($_SESSION['products']) || empty($_SESSION['products']))
     {
+        // Affiche un message si aucun produit n'est en session
         echo "<p>Aucun produit en session...</p>";
     } else 
     {
+        // Commence la construction du tableau HTML pour afficher les produits
         echo "<table class='table table-striped'>",
                 "<thead>",
                     "<tr>",
-                        "<th></th>",
-                        "<th></th>",
-                        "<th></th>",
-                        "<th></th>",
-                        "<th></th>",
-                        "<th></th>",
+                        "<th>ID</th>",
+                        "<th>Nom</th>",
+                        "<th>Prix unitaire</th>",
+                        "<th>Quantité</th>",
+                        "<th colspan='3'>Actions</th>",
                     "</tr>",
                 "</thead>",
                 "<tbody>";
         $totalGeneral = 0;
+        // Boucle à travers chaque produit dans la session pour les afficher
         foreach($_SESSION['products'] as $index => $product)
         {
             echo "<tr>",
@@ -48,15 +53,18 @@
                     "<td>".$product['name']."</td>",
                     "<td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€</td>",
                     "<td>".$product['qtt']."</td>",
+                    // Boutons pour ajuster la quantité de chaque produit
                     "<td><a class='btn btn-outline-success' href='traitement.php?action=ajouter&id=$index'>+</a></td>",
                     "<td><a class='btn btn-outline-danger' href='traitement.php?action=retirer&id=$index'>-</a></td>",
                     "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€</td>",
+                    // Bouton pour supprimer un produit du panier
                     "<td><a class='btn btn-danger' href='traitement.php?action=supprimer&id=$index'>Supprimer</a></td>",
                 "</tr>";
             $totalGeneral += $product['total'];
         }
+        // Affiche le total général des produits dans le panier
         echo "<tr>",
-                "<td colspan=4>Total général : </td>",
+                "<td colspan='6'>Total général : </td>",
                 "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;"). "&nbsp;€</strong></td>", 
             "</tr>",
             "</tbody>",
@@ -64,10 +72,11 @@
     }
     ?>
 
+    <!-- Bouton pour vider entièrement le panier -->
     <a class="btn btn-danger" href="traitement.php?action=vider">Vider le panier</a>
 
+<!-- Script Bootstrap pour assurer le bon fonctionnement des composants interactifs -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
 integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
-
--- traitement.php
+++ traitement.php
@@ -1,60 +1,78 @@
 <?php 
+    // Démarre une nouvelle session ou reprend une session existante
     session_start();
     
-        $id = (isset($_GET["id"])) ? $_GET["id"] : null;
-        // Fonction pour vider le panier en cliquant sur "supprimer"
-        if(isset($_GET['action']))
+    // Vérifie si un ID a été envoyé via GET et le stocke dans $id, sinon $id est null
+    $id = (isset($_GET["id"])) ? $_GET["id"] : null;
+    
+    // Vérifie si une action spécifique a été demandée via GET
+    if(isset($_GET['action']))
+    {
+        // Sélectionne l'action à effectuer basée sur le paramètre 'action' dans l'URL
+        switch($_GET['action'])
         {
-            switch($_GET['action'])
-            {
-                case "add": 
-                    if(isset($_POST['submit']))
+            // Cas où un produit est ajouté au panier
+            case "add": 
+                // Vérifie si le formulaire a été soumis
+                if(isset($_POST['submit']))
+                {
+                    // Nettoie et valide les données du formulaire pour éviter l'injection de code
+                    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                    $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+                    $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+
+                    // Vérifie si toutes les données du formulaire sont valides
+                    if($name && $price && $qtt)
                     {
-                        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-                        $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-                        $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+                        // Crée un tableau représentant le produit avec nom, prix, quantité, et total
+                        $product = 
+                        [
+                            "name" => $name,
+                            "price" => $price,
+                            "qtt" => $qtt,
+                            "total" => $price*$qtt
+                        ];
 
-                        if($name && $price && $qtt)
-                        {
-                            $product = 
-                            [
-                                "name" => $name,
-                                "price" => $price,
-                                "qtt" => $qtt,
-                                "total" => $price*$qtt
-                            ];
-
-                            $_SESSION['products'][] = $product;
-                        }
+                        // Ajoute le produit au tableau 'products' dans la session
+                        $_SESSION['products'][] = $product;
                     }
-                    
-                    header("Location:index.php");
-                    break;
+                }
+                
+                // Redirige l'utilisateur vers la page principale pour éviter la soumission multiple du formulaire
+                header("Location:index.php");
+                break;
 
-                case "vider":
-                    unset($_SESSION['products']);
-                    header("Location:recap.php"); die;
-                    break;
-    
-                case "supprimer":
-                    unset($_SESSION['products'][$_GET['id']]);
-                    header("Location:recap.php"); die;
-                    break;
+            // Cas pour vider entièrement le panier
+            case "vider":
+                // Supprime le tableau 'products' de la session
+                unset($_SESSION['products']);
+                // Redirige vers la page de récapitulatif et termine le script
+                header("Location:recap.php"); die;
+                break;
 
-                case "ajouter":
-                    $_SESSION['products'][$_GET['id']]['qtt']++;
-                    header("Location:recap.php"); die;
-                    break;
+            // Cas pour supprimer un produit spécifique du panier
+            case "supprimer":
+                // Supprime le produit spécifié par $id du tableau 'products'
+                unset($_SESSION['products'][$_GET['id']]);
+                // Redirige et termine le script
+                header("Location:recap.php"); die;
+                break;
 
-                case "retirer":
-                    $_SESSION['products'][$_GET['id']]['qtt']--;
-                    header("Location:recap.php"); die;
-                    break;
-            }       
-        }
+            // Cas pour augmenter la quantité d'un produit spécifique
+            case "ajouter":
+                // Incrémente la quantité du produit spécifié par $id
+                $_SESSION['products'][$_GET['id']]['qtt']++;
+                // Redirige et termine le script
+                header("Location:recap.php"); die;
+                break;
 
-    
+            // Cas pour diminuer la quantité d'un produit spécifique
+            case "retirer":
+                // Décrémente la quantité du produit spécifié par $id
+                $_SESSION['products'][$_GET['id']]['qtt']--;
+                // Redirige et termine le script
+                header("Location:recap.php"); die;
+                break;
+        }       
+    }
 ?>
-
-
-
